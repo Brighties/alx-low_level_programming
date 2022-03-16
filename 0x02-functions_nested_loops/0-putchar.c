9ba3ba95fcbf369d48dc57c7b91b5ef5596366fa
Bright Udoette <brightudoette77@gmail.com>
2022-03-15 20:47:40 -0400

    Write a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,28 +1,17 @@
-#:winclude <stdio.h>
 #include "main.h"
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
+* main - print _putchar
+*
+* Return: value 0
 */
-int main()
+int main(void)
 {
-char a = '_';
-char b = 'p';
-char c = 'u';
-char d = 't';
-char e = 'c';
-char f = 'h';
-char g = 'a';
-char k = 'r';
-    putchar(a);
-    putchar(b);
-    putchar(c);
-    putchar(d);
-    putchar(e);
-    putchar(f);
-    putchar(g);
-    putchar(k);
-    putchar('\n');
-    return 0;
+char c[8] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
 }
