440226cf453b8ade8a130e539efcb23aad30102d
Bright Udoette <brightudoette77@gmail.com>
2022-03-15 21:54:45 -0400

    Write a function that adds two integers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * add - adds two integers and returns the result
  * @a: an integer to add to b
  * @b: an integer to add to a
+ *
  * Return: Void.
  */
 
